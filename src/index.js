017317af266213b4e3a6d9fa6484e31308ff8085
smartphp <3238355967@qq.com>
2018-02-17 13:02:39 +0800

    **key**
    *** 简短说明干了什么**
     What:
    
    ---------------------------------------
    *** 为什么这么做?**
    Why:
    
    ***怎么解决的**
    How:
    
    ---------------------------------------
    
    ``
    
    ---------------------------------------

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import LogRocket from 'logrocket';
+LogRocket.init('qgqy01/demo');
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
